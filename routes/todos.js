43d2083437da32d2d838fce4d79fc771f179fa05
jstodd <jstodd2612@gmail.com>
2016-03-08 08:52:03 -0700

    latest stuff

--- routes/todos.js
+++ routes/todos.js
@@ -15,7 +15,10 @@ todosRouter.get('/', (req, res, next) => {
 });
 
 todosRouter.post('/', (req,res,next) => {
-  let newTodoId = todos.push
-})
+  Todo.create(req.body, (err, todo) => {
+    if (err) return next(err);
+    res.status(201).json(todo0;)
+  });
+});
 
 export default todosRouter;
